044c9856240e604ddc55f385b4ed69b02d987db0
unknown <selenakkaya7@gmail.com>
2022-10-26 18:05:57 +0200

    1st day task is over

--- src/plotter.py
+++ src/plotter.py
@@ -8,13 +8,13 @@ Female_Raw = arrangeData.D[:,arrangeData.L == 1]
 def plt_RawFeature(D):
     
     plt.figure()
+    i=0
     for f in range(12):
         plt.hist(Male_Raw[f, :], color = "skyblue",bins=25, density=True, alpha=0.4)
         plt.hist(Female_Raw[f, :], bins=25,color = "red", density=True, alpha=0.4)
         plt.legend(['Male','Female'])
         plt.tight_layout()
         plt.xlabel("feature_"+str(f))
-        i=0
         plt.savefig('rawDataPlots\hist_feature_'+str(i)+'_raw.jpeg')
         i=i+1
         plt.show()
